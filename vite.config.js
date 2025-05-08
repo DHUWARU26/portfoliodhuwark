5de47f2832bd5fd986d771653b4285a87cca7cc1
DHUWARU26 <103304208+DHUWARU26@users.noreply.github.com>
2025-05-08 13:56:06 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+// });
+
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: './src/app.js'
+      }
+    }
+  })
